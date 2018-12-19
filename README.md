e0a0a73d80a478ec792bdf8ffbff08fea67946b7
Patrick Hanus <patrick@skymse.com>
2018-12-19 09:42:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Signature Builder
+
+Signature Builder is a beginner app built starting with Google's Guestbook app and then going from there.
+
+## Products
+- [App Engine][1]
+
+## Language
+- [Python][2]
+
+## APIs
+- [NDB Datastore API][3]
+- [Users API][4]
+
+## Dependencies
+- [webapp2][5]
+- [jinja2][6]
+- [Twitter Bootstrap][7]
+
+[1]: https://developers.google.com/appengine
+[2]: https://python.org
+[3]: https://developers.google.com/appengine/docs/python/ndb/
+[4]: https://developers.google.com/appengine/docs/python/users/
+[5]: http://webapp-improved.appspot.com/
+[6]: http://jinja.pocoo.org/docs/
+[7]: http://twitter.github.com/bootstrap/
+
+
+## E2E Test for this sample app
+
+A Makefile is provided to deploy and run the e2e test.
+
+To run:
+
+     export GAE_PROJECT=your-project-id
+     make
+
+To manually run, install the requirements
+
+    pip install -r e2e/requirements-dev.txt
+
+Set the environment variable to point to your deployed app:
+
+    export GUESTBOOK_URL="http://guestbook-test-dot-useful-temple-118922.appspot.com/"
+
+Finally, run the test
+
+    python e2e/test_e2e.py
---
+++ sig.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START imports]
+import os
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
+import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+# [END imports]
+
+# [START main_page]
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        template_values = {
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+# [END main_page]
+
+# [START Signature Builder]
+class SignatureBuilder(webapp2.RequestHandler):
+
+    def post(self):
+        # We set the same parent key on the 'Greeting' to ensure each
+        # Greeting is in the same entity group. Queries across the
+        # single entity group will be consistent. However, the write
+        # rate to a single entity group should be limited to
+        # ~1/second.
+
+        greeting.content = self.request.get('content')
+        signature.put()
+
+        query_params = {'signature_builder': signature_builder}
+        self.redirect('/?' + urllib.urlencode(query_params))
+# [END guestbook]
+
+# [START app]
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/submit', SignatureBuilder),
+], debug=True)
+# [END app]
